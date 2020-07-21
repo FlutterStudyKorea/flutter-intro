4081b07fd729d119fe21fde485eb3295a65d8b64
jsh <ppm_it@naver.com>
2020-07-22 01:55:44 +0900

    drawer menu

--- intro/lib/main.dart
+++ intro/lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'INTRODUCE',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
       home: Grade(),
     );
   }
@@ -20,9 +23,88 @@ class Grade extends StatelessWidget {
       backgroundColor: Colors.amber[700],
       appBar: AppBar(
         title: Text('INTRODUCE'),
-        backgroundColor: Colors.amber[600],
         centerTitle: true,
         elevation: 0.0,
+
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: (){
+              print('Shopping cart button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              print('Search button is clicked');
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/sehee.jpeg'),
+                backgroundColor: Colors.white,
+              ),
+//              otherAccountsPictures: <Widget>[
+//                CircleAvatar(
+//                  backgroundImage: AssetImage('assets/daco.png'),
+//                  backgroundColor: Colors.white,
+//                ),
+//                CircleAvatar(
+//                  backgroundImage: AssetImage('assets/daco.png'),
+//                  backgroundColor: Colors.white,
+//                ),
+             // ],
+              accountName: Text('jsh-me'),
+              accountEmail: Text('ppm_it@naver.com'),
+              onDetailsPressed: (){
+                print('clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.amber[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20.0),
+                  bottomRight: Radius.circular(20.0),
+                )
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home,
+              color: Colors.grey[800],
+              ),
+              title: Text('Home'),
+              trailing: Icon(Icons.navigate_next),
+              onTap: (){
+                print('Home is clicked');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings,
+                color: Colors.grey[800],
+              ),
+              title: Text('Setting'),
+              trailing: Icon(Icons.navigate_next),
+              onTap: (){
+                print('Setting is clicked');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer,
+                color: Colors.grey[800],
+              ),
+              title: Text('QnA'),
+              trailing: Icon(Icons.navigate_next),
+              onTap: (){
+                print('QnA is clicked');
+              },
+            ),
+          ],
+        ),
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -148,4 +230,9 @@ class Grade extends StatelessWidget {
       ),
     );
   }
+}
+
+void showSnackBar(String msg, BuildContext context){
+  Scaffold.of(context).showSnackBar(
+      SnackBar(content: Text(msg)));
 }
